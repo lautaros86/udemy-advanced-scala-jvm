dba5a986f32551bcd395a472910a7c077d4dfb6d
Lautaro Silva <lautaro.silva@despegar.com>
2022-02-20 19:51:15 -0300

    first commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "udemy-advanced-jvm-scala"
+
+version := "0.1"
+
+scalaVersion := "3.1.1"
---
+++ src/main/scala/playground/Playground.scala
@@ -0,0 +1,9 @@
+package playground
+
+object Playground {
+
+  def main(args: Array[String]): Unit = {
+    println("I'm so readu to becom a Scala rockstar!")
+  }
+
+}
